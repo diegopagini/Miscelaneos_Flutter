4235e82589ec82dd9a28b12e74f741897a737d2d
Diego Pagini <ext_atsistemas10@es.gestamp.com>
2024-02-23 11:38:01 +0100

    assets

--- lib/config/plugins/quick_actions.plugin.dart
+++ lib/config/plugins/quick_actions.plugin.dart
@@ -1,3 +1,4 @@
+import 'package:miscelaneos/config/config.dart';
 import 'package:quick_actions/quick_actions.dart';
 
 class QuickActionsPlugin {
@@ -8,24 +9,32 @@ class QuickActionsPlugin {
       // Only 4 actions are allowed
       switch (type) {
         case 'biometric':
+          router.push('/biometrics');
           break;
 
         case 'compass':
+          router.push('/compass');
           break;
 
         case 'pokemons':
+          router.push('/pokemons');
           break;
 
         case 'charmander':
+          router.push('/pokemons/4');
           break;
       }
     });
 
     quickActions.setShortcutItems([
-      const ShortcutItem(type: 'biometric', localizedTitle: 'Biometric'),
-      const ShortcutItem(type: 'compass', localizedTitle: 'Compass'),
-      const ShortcutItem(type: 'pokemons', localizedTitle: 'Pokemons'),
-      const ShortcutItem(type: 'charmander', localizedTitle: 'Charmander'),
+      const ShortcutItem(
+          type: 'biometric', localizedTitle: 'Biometric', icon: 'finger'),
+      const ShortcutItem(
+          type: 'compass', localizedTitle: 'Compass', icon: 'compass'),
+      const ShortcutItem(
+          type: 'pokemons', localizedTitle: 'Pokemons', icon: 'pokemons'),
+      const ShortcutItem(
+          type: 'charmander', localizedTitle: 'Charmander', icon: 'charmander'),
     ]);
   }
 }
